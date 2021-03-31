a56a3091eeff75137fa9a58c20d83adc166add25
MnqobiSB <amandlamm1@gmail.com>
2021-03-31 18:05:19 +0200

    modify app.js & package.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
 import React, { /*Component*/ useState, useEffect } from 'react';
 
 const App = () => {
-	const [ count, setCount ] = useState(0);
-
-	useEffect(() => {
-		document.title = `Clicked ${count} times`;
-	});
-
-	const increament = () => {
-		setCount(count + 1);
+	// state
+	const [ news, setNews ] = useState([]);
+	const [ searchQuery, setSearchQuery ] = useState('react');
+	const [ url, setUrl ] = useState(
+		'http://hn.algolia.com/api/v1/search?query=react'
+	);
+	const [ loading, setLoading ] = useState(false);
+	// fetch news
+	const fetchNews = () => {
+		// set loading true
+		setLoading(true);
+		fetch(url)
+			.then((result) => result.json())
+			// .then((data) => console.log(data));
+			// eslint-disable-next-line no-sequences
+			.then((data) => (setNews(data.hits), setLoading(false)))
+			.catch((error) => console.log(error));
 	};
+
+	useEffect(
+		() => {
+			fetchNews();
+		},
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[ url ]
+	);
+
+	const handleChange = (e) => {
+		setSearchQuery(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+	};
+
+	const showLoading = () => (loading ? <h2>Loading...</h2> : '');
+
+	const searchForm = () => {
+		return (
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={searchQuery}
+					onChange={handleChange}
+				/>
+				<button>Search</button>
+			</form>
+		);
+	};
+
+	const showNews = () => news.map((n, i) => <p key={i}>{n.title}</p>);
 
 	return (
 		<div>
-			<h2>Counter App</h2>
-			<button onClick={increament}>Clicked {count} times</button>
+			<h2>News</h2>
+			{showLoading()}
+			{searchForm()}
+			{showNews()}
 		</div>
 	);
 };
+
+// const App = () => {
+// 	const [ count, setCount ] = useState(0);
+
+// 	useEffect(() => {
+// 		document.title = `Clicked ${count} times`;
+// 	});
+
+// 	const increament = () => {
+// 		setCount(count + 1);
+// 	};
+
+// 	return (
+// 		<div>
+// 			<h2>Counter App</h2>
+// 			<button onClick={increament}>Clicked {count} times</button>
+// 		</div>
+// 	);
+// };
 
 // class App extends Component {
 // 	state = {
